749d3f191134aa29f8333e612e5c1e1c5cd1d7b0
Soren Hein <soren.hein@gmail.com>
2017-06-07 08:59:56 +0200

    Added Instance.  It compiles but it not hooked up

---
+++ src/Instance.cpp
@@ -0,0 +1,477 @@
+/* 
+   Part of BridgeData.
+
+   Copyright (C) 2016-17 by Soren Hein.
+
+   See LICENSE and README.
+*/
+
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+#include "Instance.h"
+#include "bconst.h"
+#include "parse.h"
+#include "Bexcept.h"
+#include "Bdiff.h"
+
+
+Instance::Instance()
+{
+  Instance::reset();
+}
+
+
+Instance::~Instance()
+{
+}
+
+
+void Instance::reset()
+{
+  players.reset();
+  auction.reset();
+  contract.reset();
+  play.reset();
+
+  LINset = false;
+}
+
+
+void Instance::setLINheader(const LINInstData& lin)
+{
+  if (LINset)
+    return;
+
+  LINdata = lin;
+  LINset = true;
+
+  if (LINdata.contract != "")
+    Instance::setContract(LINdata.contract, BRIDGE_FORMAT_LIN);
+
+  const string st = strPlayersFromLINHeader();
+  if (st != ",,,")
+    Instance::setPlayers(st, BRIDGE_FORMAT_LIN, false);
+}
+
+
+void Instance::setDealer(
+  const string& text,
+  const Format format)
+{
+  auction.setDealer(text, format);
+}
+
+
+void Instance::setVul(
+  const string& text,
+  const Format format)
+{
+  auction.setVul(text, format);
+  contract.setVul(auction.getVul());
+}
+
+
+// Auction
+
+void Instance::addCall(
+  const string& call,
+  const string& alert)
+{
+  auction.addCall(call, alert);
+}
+
+
+void Instance::addAlert(
+  const unsigned alertNo,
+  const string& alert)
+{
+  auction.addAlert(alertNo, alert);
+}
+
+
+void Instance::addPasses()
+{
+  auction.addPasses();
+}
+
+
+void Instance::undoLastCall()
+{
+  auction.undoLastCall();
+}
+
+
+void Instance::passOut()
+{
+  contract.passOut();
+}
+
+
+void Instance::setAuction(
+  const string& text,
+  const Format format)
+{
+  auction.addAuction(text, format);
+
+  if (auction.hasDealerVul())
+    contract.setVul(auction.getVul());
+
+  // Doesn't bother us unduly if there is no contract here.
+  if (auction.getContract(contract))
+    play.setContract(contract);
+}
+
+
+bool Instance::hasDealerVul() const
+{
+  return auction.hasDealerVul();
+}
+
+
+bool Instance::auctionIsOver() const
+{
+  return auction.isOver();
+}
+
+
+bool Instance::auctionIsEmpty() const
+{
+  return auction.isEmpty();
+}
+
+
+bool Instance::isPassedOut() const
+{
+  return auction.isPassedOut();
+}
+
+
+unsigned Instance::lengthAuction() const
+{
+  return auction.length();
+}
+
+
+// Contract
+
+void Instance::setContract(
+  const Vul vul,
+  const string& cstring)
+{
+  contract.setContract(vul, cstring);
+}
+
+
+void Instance::setContract(
+  const string& text,
+  const Format format)
+{
+  contract.setContract(text, format);
+  play.setContract(contract);
+}
+
+
+void Instance::setDeclarer(
+  const string& text,
+  const Format format)
+{
+  UNUSED(format);
+  contract.setDeclarer(text);
+}
+
+
+bool Instance::contractIsSet() const
+{
+  return contract.isSet();
+}
+
+
+void Instance::setScore(
+  const string& text,
+  const Format format)
+{
+  contract.setScore(text, format);
+}
+
+
+void Instance::calculateScore()
+{
+  contract.calculateScore();
+}
+
+
+// Play
+
+void Instance::setPlays(
+  const string& text,
+  const Format format)
+{
+  play.setPlays(text, format);
+
+  if (play.isOver())
+    contract.setTricks(play.getTricks());
+}
+
+
+void Instance::undoLastPlay()
+{
+  play.undoPlay();
+}
+
+
+bool Instance::playIsOver() const
+{
+  return play.isOver();
+}
+
+
+bool Instance::hasClaim() const
+{
+  return play.hasClaim();
+}
+
+
+// Result
+
+void Instance::setResult(
+  const string& text,
+  const Format format)
+{
+  contract.setResult(text, format);
+
+  if (! contract.isPassedOut())
+    play.makeClaim(contract.getTricks());
+}
+
+
+bool Instance::hasResult() const
+{
+  return contract.hasResult();
+}
+
+
+// Players
+
+void Instance::setPlayers(
+  const string& text,
+  const Format format,
+  const bool hardFlag)
+{
+  players.set(text, format, hardFlag);
+}
+
+
+void Instance::setPlayer(
+  const string& text,
+  const Player player)
+{
+  players.setPlayer(text, player);
+}
+
+
+void Instance::copyPlayers(const Instance& inst2)
+{
+  players = inst2.players;
+}
+
+
+unsigned Instance::missingPlayers() const
+{
+  return players.missing();
+}
+
+
+bool Instance::overlappingPlayers(const Instance& inst2) const
+{
+  return players.overlap(inst2.players);
+}
+
+
+void Instance::setRoom(
+  const string& text,
+  const Format format)
+{
+  players.setRoom(text, format);
+}
+
+
+bool Instance::operator == (const Instance& inst2) const
+{
+  if (players != inst2.players)
+    return false;
+  if (auction != inst2.auction)
+    return false;
+  if (contract != inst2.contract)
+    return false;
+  if (play != inst2.play)
+    return false;
+
+  return true;
+}
+
+
+bool Instance::operator != (const Instance& inst2) const
+{
+  return ! (* this == inst2);
+}
+
+
+string Instance::strDealer(const Format format) const
+{
+  return auction.strDealer(format);
+}
+
+
+string Instance::strVul(const Format format) const
+{
+  return contract.strVul(format);
+}
+
+
+string Instance::strAuction(const Format format) const
+{
+  if (format == BRIDGE_FORMAT_TXT)
+  {
+    int lengths[BRIDGE_PLAYERS];
+    for (unsigned p = 0; p < BRIDGE_PLAYERS; p++)
+    {
+      Player pp = PLAYER_DDS_TO_TXT[p];
+      lengths[p] = static_cast<int>
+        (players.strPlayer(pp, format).length());
+      lengths[p] = Max(12, lengths[p]+1);
+    }
+    return auction.str(format, lengths);
+  }
+  else
+    return auction.str(format);
+}
+
+
+string Instance::strContract(const Format format) const
+{
+  return contract.str(format);
+}
+
+
+string Instance::strDeclarer(const Format format) const
+{
+  return contract.strDeclarer(format);
+}
+
+
+string Instance::strTricks(const Format format) const
+{
+  return contract.strTricks(format);
+}
+
+
+string Instance::strScore(
+  const Format format,
+  const int refScore) const
+{
+  return contract.strScore(format, refScore);
+}
+
+
+string Instance::strScoreIMP(
+  const Format format,
+  const int refScore) const
+{
+  return contract.strScoreIMP(format, refScore);
+}
+
+
+int Instance::IMPScore(const int refScore) const
+{
+  return contract.IMPScore(refScore);
+}
+
+
+string Instance::strLead(const Format format) const
+{
+  return play.strLead(format);
+}
+
+
+string Instance::strPlay(const Format format) const
+{
+  return play.str(format);
+}
+
+
+string Instance::strClaim(const Format format) const
+{
+  return play.strClaim(format);
+}
+
+
+string Instance::strPlayer(
+  const Player player,
+  const Format format) const
+{
+  return players.strPlayer(player, format);
+}
+
+
+string Instance::strPlayers(const Format format) const
+{
+  return players.str(format);
+}
+
+
+string Instance::strPlayersFromLINHeader() const
+{
+  string st;
+  for (unsigned i = 0; i < BRIDGE_PLAYERS; i++)
+    st += LINdata.players[PLAYER_DDS_TO_LIN[i]] + ",";
+  st.pop_back(); // Trailing comma
+  return st;
+}
+
+
+string Instance::strPlayersDelta(
+  const Instance * refInstance,
+  const Format format) const
+{
+  if (refInstance == nullptr)
+    return players.str(BRIDGE_FORMAT_LIN_RP) + ",";
+  else
+    return players.strDelta(refInstance->players, format);
+}
+
+
+string Instance::strResult(
+  const Format format,
+  const int refScore) const
+{
+  return contract.strResult(format, refScore);
+}
+
+
+string Instance::strResult(
+  const Format format,
+  const string& team,
+  const int refScore) const
+{
+  contract.strResult(format, refScore, team);
+}
+
+
+string Instance::strResultEntry() const
+{
+  stringstream ss;
+  ss << setw(8) << left << contract.str(BRIDGE_FORMAT_LIN);
+
+  const int score = contract.getScore();
+  if (score == 0)
+    ss << "     --     --";
+  else if (score > 0)
+    ss << setw(7) << right << score << setw(7) << "";
+  else
+    ss << setw(14) << right << -score;
+  return ss.str();
+}
+
---
+++ src/Instance.h
@@ -0,0 +1,201 @@
+/* 
+   Part of BridgeData.
+
+   Copyright (C) 2016-17 by Soren Hein.
+
+   See LICENSE and README.
+*/
+
+
+#ifndef BRIDGE_INSTANCE_H
+#define BRIDGE_INSTANCE_H
+
+#include <string>
+
+#include "Players.h"
+#include "Auction.h"
+#include "Contract.h"
+#include "Play.h"
+
+using namespace std;
+
+
+class Instance
+{
+  private:
+
+    Players players;
+    Auction auction;
+    Contract contract;
+    Play play;
+
+    LINInstData LINdata;
+    bool LINset;
+
+    string strResultEntry() const;
+
+
+  public:
+
+    Instance();
+
+    ~Instance();
+
+    void reset();
+
+    void setLINheader(const LINInstData& lin);
+
+    void setDealer(
+      const string& text,
+      const Format format);
+
+    void setVul(
+      const string& text,
+      const Format format);
+
+    // Deal
+
+    void setDeal(
+      const string& text,
+      const Format format);
+      
+    // Auction
+
+    void addCall(
+      const string& call,
+      const string& alert = "");
+
+    void addAlert(
+      const unsigned alertNo,
+      const string& alert);
+
+    void addPasses();
+
+    void undoLastCall();
+
+    void passOut();
+
+    void setAuction(
+      const string& text,
+      const Format format);
+
+    bool auctionIsEmpty() const;
+
+    bool hasDealerVul() const;
+
+    bool auctionIsOver() const;
+
+    bool isPassedOut() const;
+
+    unsigned lengthAuction() const;
+
+    // Contract
+
+    void setContract(
+      const Vul vul,
+      const string& cstring);
+
+    void setContract(
+      const string& text,
+      const Format format);
+
+    void setDeclarer(
+      const string& text,
+      const Format format);
+
+    bool contractIsSet() const;
+
+    void setScore(
+      const string& text,
+      const Format format);
+
+    void calculateScore();
+
+    // Play
+
+    void setPlays(
+      const string& text,
+      const Format format);
+
+    void undoLastPlay();
+
+    bool playIsOver() const;
+
+    bool hasClaim() const;
+
+    // Result
+
+    void setResult(
+      const string& text,
+      const Format format);
+      
+    bool hasResult() const;
+
+    // Players
+
+    void setPlayers(
+      const string& text,
+      const Format format,
+      const bool hardFlag = true);
+
+    void setPlayer(
+      const string& text,
+      const Player player);
+
+    void copyPlayers(const Instance& inst2);
+
+    unsigned missingPlayers() const;
+    bool overlappingPlayers(const Instance& inst2) const;
+
+    void setRoom(
+      const string& s,
+      const Format format);
+
+    bool operator == (const Instance& inst2) const;
+    bool operator != (const Instance& inst2) const;
+
+    string strDealer(const Format format) const;
+    string strVul(const Format format) const;
+    string strAuction(const Format format) const;
+    string strContract(const Format format) const;
+    string strDeclarer(const Format format) const;
+    string strTricks(const Format format) const;
+
+    string strScore(
+      const Format format,
+      const int refScore) const;
+
+    string strScoreIMP(
+      const Format format,
+      const int refScore) const;
+
+    int IMPScore(const int refScore) const;
+
+    string strLead(const Format format) const;
+    string strPlay(const Format format) const;
+    string strClaim(const Format format) const;
+    
+    string strPlayer(
+      const Player player,
+      const Format format) const;
+    
+    string strPlayers(const Format format) const;
+
+    string strPlayersDelta(
+      const Instance * refInstance,
+      const Format format) const;
+
+    string strPlayersFromLINHeader() const;
+
+    string strResult(
+      const Format format,
+      const int refScore) const;
+
+    string strResult(
+      const Format format,
+      const string& team,
+      const int refScore) const;
+};
+
+#endif
+
